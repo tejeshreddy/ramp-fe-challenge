b2ba2b8b3c46c1745d8ffe2b9e813f4ec11c4a05
tejeshreddy <tejeshreddy111@gmail.com>
2023-10-03 11:30:27 -0500

    fix: bug 2

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -6,21 +6,27 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
   const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
 
   return (
-    <div className="RampInputCheckbox--container" data-testid={inputId}>
-      <label
-        className={classNames("RampInputCheckbox--label", {
-          "RampInputCheckbox--label-checked": checked,
-          "RampInputCheckbox--label-disabled": disabled,
-        })}
-      />
-      <input
-        id={inputId}
-        type="checkbox"
-        className="RampInputCheckbox--input"
-        checked={checked}
-        disabled={disabled}
-        onChange={() => onChange(!checked)}
-      />
+    <div
+      className="RampInputCheckbox--container"
+      data-testid={inputId}
+      onClick={() => onChange(!checked)}
+    >
+      <div>
+        <label
+          className={classNames("RampInputCheckbox--label", {
+            "RampInputCheckbox--label-checked": checked,
+            "RampInputCheckbox--label-disabled": disabled,
+          })}
+        />
+        <input
+          id={inputId}
+          type="checkbox"
+          className="RampInputCheckbox--input"
+          checked={checked}
+          disabled={disabled}
+          onChange={() => onChange(!checked)}
+        />
+      </div>
     </div>
   )
 }
