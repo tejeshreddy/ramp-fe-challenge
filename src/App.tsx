441f7a6a6018ca96330727be0d7a87925fba812b
tejeshreddy <tejeshreddy111@gmail.com>
2023-10-03 13:16:15 -0500

    fix: bug-6 and bug-7

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ export function App() {
         <div className="RampGrid">
           <Transactions transactions={transactions} />
 
-          {transactions !== null && (
+          {transactions !== null && paginatedTransactions?.nextPage && (
             <button
               className="RampButton"
               disabled={paginatedTransactionsUtils.loading}
